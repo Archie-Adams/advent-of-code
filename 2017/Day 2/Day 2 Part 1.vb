1bd28c02d0c0ce08033fbbc8524c675759a55402
Archie Adams <archiea2002@gmail.com>
2020-06-20 19:35:28 +0100

    2017 Day 2 finished

---
+++ 2017/Day 2/Day 2 Part 1.vb
@@ -0,0 +1,26 @@
+﻿Imports System.IO.File
+Imports System.Collections.Generic
+
+Module Module1
+
+    Sub Main()
+
+        dim fileContents as List(of String) = ReadAllLines("input.csv").ToList
+        dim checkSum as Integer = 0
+
+        for i = 0 To 15 ' For each line of the file.
+            dim item as String() = fileContents(i).Split(",")
+            dim largest as Integer = 0
+            dim smallest as Integer = 99999
+                for x = 0 to 15 ' For each number in the line.
+                    if item(x) > largest then largest = item(x)
+                    if item(x) < smallest then smallest = item(x)
+                next
+                checkSum += largest - smallest
+        next
+
+        Console.WriteLine(checkSum)
+
+    End Sub
+
+End Module
---
+++ 2017/Day 2/Day 2 Part 2.vb
@@ -0,0 +1,35 @@
+﻿Imports System.IO.File
+Imports System.Collections.Generic
+
+Module Module1
+
+    Sub Main()
+
+        dim total as Integer = 0     
+        dim fileContents as List(of String) = ReadAllLines("input.csv").ToList
+
+        for i = 0 To 15 ' For each line of the file.
+            dim item as String() = fileContents(i).Split(",")
+
+                for x = 0 to 15 ' For each item of the line.              
+                    dim foundLinesValue as Boolean = False         
+
+                    for z = 0 to 15 ' Compare to every other item.
+                        if item(x) <> item(z) then
+                            if item(x) Mod item(z) = 0 then
+                                total += item(x) / item(z)
+                                foundLinesValue = True
+                                exit for
+                            end if
+                        end if
+                    next
+
+                    if foundLinesValue = True then exit for
+                next                
+        next
+
+        Console.WriteLine(total)
+
+    End Sub
+
+End Module
