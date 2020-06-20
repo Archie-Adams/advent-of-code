13b9bddc7dcd5239309e7d50667003b028e36d46
Archie Adams <archiea2002@gmail.com>
2020-06-20 20:30:24 +0100

    2017 Day 4 Part 1 finished

--- 2017/Day 4 - Unrevised/Day 4 Part 1.vb
+++ 2017/Day 4 - Unrevised/Day 4 Part 1.vb
@@ -1,48 +1,36 @@
-﻿Module Module1
+﻿imports System.IO.File
+Imports System.Collections.Generic
+
+Module Module1
 
     Sub Main()
+        
+        Dim input As List(Of String) = ReadAllLines("input.txt").ToList
+        Dim acceptedPasscodes As Integer = 0
 
-        Dim csvfile As String = My.Application.Info.DirectoryPath & "\input.txt" 'loaction of file
-        Dim filecontents As List(Of String) = System.IO.File.ReadAllLines(csvfile).ToList 'Every line of text file in list
-        Dim numberOfAcceptedPasscodes As Integer = 0 'var to count how many are accepted
+        For Each line In input
 
-        For Each item In filecontents 'loops for every line of file
+            ' Split line on spaces to an array
+            dim words as string() = line.split(new string() {" "}, stringsplitoptions.none)
 
-            Dim boolNotAccepted As Boolean = False
-            Dim noOfSpaces As Integer = CountCharacter(item, " ") 'finds out how man spaces are in that line
-            Dim splitters As String() = item.Split(" ") 'char to split the string at
-            Dim arrOfWordsInLine(noOfSpaces) As String 'array to store each word in the line
+            ' Check if all values in the array are unique
+            dim matchingWords as boolean = false
+            for x = 0 to words.count - 1
+                for i = 0 to words.count - 1
+                    if words(x) = words(i) and x <> i then 
+                        matchingWords = true
+                        exit for
+                    end if
+                next
+                if matchingWords = true then exit for
+            next
 
-            For i = 0 To noOfSpaces
-                arrOfWordsInLine(i) = splitters(i) 'places all of words on line into array
-            Next
+            if matchingWords = false then acceptedPasscodes += 1            
 
-            For a = 0 To noOfSpaces 'loops the length of the array
-                Dim wordToCheckAgainst As String = arrOfWordsInLine(a)
-                Dim wordToCheckAgainstIndex As Integer = a
+        next  
 
-                For b = 0 To noOfSpaces
-                    If wordToCheckAgainst = arrOfWordsInLine(b) And b <> wordToCheckAgainstIndex Then boolNotAccepted = True : Exit For
-                Next
-                If boolNotAccepted = True Then Exit For
-
-            Next
-            If boolNotAccepted = False Then numberOfAcceptedPasscodes += 1
-
-        Next
-
-        MsgBox(numberOfAcceptedPasscodes)
+        console.writeline(acceptedPasscodes)
 
     End Sub
-
-    Public Function CountCharacter(ByVal value As String, ByVal ch As Char) As Integer ''counts number of char occurances in a string
-        Dim cnt As Integer = 0
-        For Each c As Char In value
-            If c = ch Then
-                cnt += 1
-            End If
-        Next
-        Return cnt
-    End Function
 
 End Module
