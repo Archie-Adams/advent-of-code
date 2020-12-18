ad795973694fdc2c9c2c84ded9c84b89d3d0c753
Archie Adams <archiea2002@gmail.com>
2020-12-18 21:31:49 +0000

    more descriptive comments

--- 2020/day-7/part1.py
+++ 2020/day-7/part1.py
@@ -10,14 +10,14 @@ def canContainShinyGold(colour):
     if colour in contains_gold or colour == "shiny gold":
         return True
 
-    # For each sub colour the given colour can hold.
+    # For each sub-colour the given colour can hold.
     for sub_colour in rules[[item[0] for item in rules].index(colour)][1:]:
         if canContainShinyGold(sub_colour[2:]):
             contains_gold.add(sub_colour[2:])
             return True
 
 
-# Parse rules to a list.
+# Parse input to a list of lists; [[colour, digit colour, digit colour,..],..].
 rgx = r'(^(?:\w+\s\w+){1,2}|\d+\s\w+\s\w+)'
 rules = [re.findall(rgx, line) for line in open('input.txt')]
 
