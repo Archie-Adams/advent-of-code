52445f6e181db18523e8eb16145f40d0f47ab401
Archie Adams <archiea2002@gmail.com>
2020-12-23 19:09:24 +0000

    Removed Trailing Whitespace

--- 2017/Day 2/Day 2 Part 2.vb
+++ 2017/Day 2/Day 2 Part 2.vb
@@ -8,14 +8,14 @@ Module Module1
 
     Sub Main()
 
-        dim total as Integer = 0     
+        dim total as Integer = 0
         dim fileContents as List(of String) = ReadAllLines("input.csv").ToList
 
         for i = 0 To 15 ' For each line of the file.
             dim item as String() = fileContents(i).Split(",")
 
-                for x = 0 to 15 ' For each item of the line.              
-                    dim foundLinesValue as Boolean = False         
+                for x = 0 to 15 ' For each item of the line.
+                    dim foundLinesValue as Boolean = False
 
                     for z = 0 to 15 ' Compare to every other item.
                         if item(x) <> item(z) then
@@ -28,7 +28,7 @@ Module Module1
                     next
 
                     if foundLinesValue = True then exit for
-                next                
+                next
         next
 
         Console.WriteLine(total)
